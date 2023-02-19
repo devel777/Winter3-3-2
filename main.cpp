a6552dd2395c2c72a39a6fbc8482084622d32275
dev <mikhail.artemkin@gmail.com>
2023-02-19 10:26:59 +0300

    Переименование переменных

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ cout << "Задан массив 10x3, состоящий из вещественных элементов. \n"
         "Найти количество элементов, абсолютное значение которых больше среднего арифметического." << endl;
 cout << "\n";
 
-float a[10][3],sum,average;
-int kol,kol2,i,j;
+float a[10][3],sum,avg;
+int count1,count2,i,j;
 srand(time(0));
 cout << "Array a:" << endl;
 
@@ -29,19 +29,19 @@ cout << "Array a:" << endl;
     }
     cout <<"\n"; // Софрмировали и вывели 2-мерный массив
     cout << "Коли-во элементов массива: " << i*j << endl;
-    kol=i*j;
-    kol2=0;
+    count1=i*j;
+    count2=0;
     sum=0;
-    average=0;
+    avg=0;
         for (i = 0; i < 10; i++)
             for (j = 0; j < 3; j++)
                     sum+= a[i][j];
-                average=sum/kol;
+                avg=sum/count1;
                 cout << "Сумма элементов массива: " << sum << endl;
-                cout << "Среднее арифм элементов массива: " << average << endl;
+                cout << "Среднее арифм элементов массива: " << avg << endl;
         for (i = 0; i < 10; i++)
             for (j = 0; j < 3; j++)
-        if (abs(a[i][j]) > average) kol2++;
-        cout<< "Количество элементов больше абсолютного среднего арифметического "  << kol2 << endl;
+        if (abs(a[i][j]) > avg) count2++;
+        cout<< "Количество элементов больше абсолютного среднего арифметического "  << count2 << endl;
     return 0;
 }
